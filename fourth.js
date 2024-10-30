4b699ab9c76627a20c72b415f82ba6facbf291b0
Jethanandj52 <134337163+Jethanandj52@users.noreply.github.com>
2024-10-30 23:32:13 +0500

    Add files via upload

---
+++ fourth.js
@@ -0,0 +1,22 @@
+// first
+var a=10, b=20, c=30;
+
+alert("Question 1 \n The Value of a=10, b=20, c=30 and a+b*c: "+a+b*c)
+
+// second
+
+// legal variable
+var firstname, lastName, your_age, $date, _number;
+
+// illegal varaible
+
+// var alert, first name, last-name,123name;
+
+// third
+var number='numbers', dollor='$', under_score='_', name1="$name",name2="_name",name3='name',letter="letters",caseSensitive="case sensitie", keyWords='Keywords';
+
+document.write("A)Variables names can only contain,"+number+" , "+dollor+" and "+under_score+" . For Example "+name1+" "+name2+" "+name3)
+document.write("<br>B)Varibles must begin start with"+letter+","+dollor+"and"+under_score+" . For Example "+name1+" "+name2+" "+name3)
+document.write("<br>C) Variables names are case"+caseSensitive+".");
+document.write("<br>D) Variables names should not be JS" +keyWords+".")
+ 
